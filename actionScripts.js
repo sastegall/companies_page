467103fff0ec1e0bf0cf0f3dc8ad0bb183e62f16
Abby Stegall <astegall@Acorn.com>
2020-11-11 14:10:00 -0500

    initial commit

---
+++ actionScripts.js
@@ -0,0 +1,163 @@
+function Element(tag, top, bottom, left, right){
+  this.id = tag;
+  this.top = top;
+  this.bottom = bottom;
+  this.left = left;
+  this.right = right;
+}
+
+let activeElement;
+var allListedElements = [];
+var allDivisionElements = [];
+var currentScrollY = 0;
+
+gatherElements()
+
+function compOnClick(element){
+  var clickedElement = document.getElementById(element);
+  console.log(clickedElement);
+  currentScrollY = window.scrollY;
+  console.log("currentScrollY " + currentScrollY);
+
+  if(activeElement !=  undefined){
+    //setTimeout(collapseDivsion, 500);
+    collapseDivsion();
+    activeElement = undefined;
+    console.log(activeElement);
+  }
+
+  if(clickedElement.id != "jrsmith" || clickedElement.id != "acorn-engineering"){
+    var clickedIndex = allListedElements.indexOf(clickedElement.id)
+    if (clickedIndex % 2 == 1){
+      var priorElement = document.getElementById(allListedElements[clickedIndex - 1]);
+      priorElement.classList.remove("col-md-6")
+    }
+    clickedElement.classList.remove("col-md-6")
+  }
+
+  if (activeElement == undefined){
+    console.log("new element clicked");
+    clickedElement.classList.add("division-clicked");
+    var clickedContentId = clickedElement.id + "-content"
+    var clickedGradient  = document.getElementById(clickedElement.id + "-gradient");
+    clickedGradient.classList.add("division-title-gradient-clicked");
+    var divisionTitle = document.getElementById(clickedElement.id + "-title-row")
+    divisionTitle.classList.remove("division-title-visible");
+    divisionTitle.classList.add("division-title-hidden");
+    document.getElementById(clickedContentId).classList.remove("division-content-hidden")
+    console.log(document.getElementById(clickedContentId));
+    var testEl =  allDivisionElements.forEach(item => {
+      if (clickedElement == item.id){
+        activeElement = item;
+      }
+    });
+  //  console.log(activeElement);
+    //activeElement = allDivisionElements[allDivisionElements.indexOf(clickedElement.id)]
+  }
+}
+
+
+
+function  gatherElements(){
+    allMajorElements = document.getElementsByClassName('major-division');
+    allMinorElements = document.getElementsByClassName('minor-division');
+    Array.prototype.forEach.call(allMajorElements, function(element){
+      var divElement = new Element(element, element.getBoundingClientRect().top,
+                                  element.getBoundingClientRect().bottom, element.getBoundingClientRect().left,
+                                  element.getBoundingClientRect().right)
+     allDivisionElements.push(divElement);
+    });
+
+    Array.prototype.forEach.call(allMinorElements, function(element) {
+        allListedElements.push(element.id)
+
+        var divElement = new Element(element, element.getBoundingClientRect().top,
+                                    element.getBoundingClientRect().bottom, element.getBoundingClientRect().left,
+                                    element.getBoundingClientRect().right)
+        allDivisionElements.push(divElement);
+    });
+  }
+
+
+
+
+
+function collapseDivsion(){
+  if (activeElement.id.id != "jrsmith" && activeElement.id.id != "acorn-engineering"){
+    console.log(activeElement.id.id);
+    console.log("minor division");
+    activeElement.id.classList.add("col-md-6")
+
+    var clickedIndex = allListedElements.indexOf(activeElement.id.id)
+    if (clickedIndex % 2 == 1){
+      var priorElement = document.getElementById(allListedElements[clickedIndex - 1]);
+      priorElement.classList.add("col-md-6")
+    }
+  }
+
+  document.getElementById(activeElement.id.id).classList.remove("division-clicked")
+  var activeGradient  = document.getElementById(activeElement.id.id + "-gradient");
+  activeGradient.classList.remove("division-title-gradient-clicked");
+
+  var activeTitle = document.getElementById(activeElement.id.id + "-title-row")
+  activeTitle.classList.add("division-title-visible");
+  activeTitle.classList.remove("division-title-hidden");
+
+  var activeContentId = activeElement.id.id + "-content"
+  // document.getElementById(activeContentId).classList.add("division-content-hidden")
+
+  console.log('collapseDivsion called');
+  activeElement.id.classList.remove("division-clicked");
+  document.getElementById(activeContentId).classList.add("division-content-hidden")
+  activeElement = undefined;
+}
+
+
+window.addEventListener("scroll", function(){
+console.log("scroll event");
+  if(activeElement != undefined){
+
+
+//basically when a link is clicked it causes the page to scroll. The compClick funciton deosn't have time to catch up so the wrong division is collapsed
+
+    if (window.scrollY > currentScrollY + 1300){
+      console.log("down");
+      console.log(window.scrollY);
+      collapseDivsion();
+    }
+
+    if (window.scrollY < currentScrollY - 1400){
+      console.log("up");
+      console.log(window.scrollY);
+      collapseDivsion();
+    }
+
+
+
+    // if((allListedElements.includes(activeElement.id.id)) && (allListedElements.indexOf(activeElement.id.id) == allListedElements.length - 1 || allListedElements.indexOf(activeElement.id.id) == allListedElements.length - 2)){
+    //
+    //   if ((activeElement.bottom) < window.scrollY - 700){
+    //   collapseDivsion(activeElement);
+    //   }
+    //
+    //   if((activeElement.top) > window.scrollY + 250){
+    //   collapseDivsion(activeElement);
+    // }
+    // }
+    // else
+    // {
+    //   if ((activeElement.bottom) < window.scrollY - 300){
+    //   collapseDivsion(activeElement);
+    //   }
+    //
+    //   if((activeElement.top) > window.scrollY - 250){
+    //   collapseDivsion(activeElement);
+    //
+    // }
+    //
+    // }
+}
+})
+
+
+//classList.add(col-xl-6)
